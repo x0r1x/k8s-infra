e9c23deefe273f07e5593b33f901b5afdbe83a92
Kashin Aleksey <kashin.aleksey@gmail.com>
2025-04-25 18:57:27 +0300

    init

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = "~>1.9.5"
+
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    key                         = "terraform/state.tfstate" # Путь к файлу состояния
+    region                      = "ru-central1"
+    bucket                      = var.yc_s3_bucket
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+  }
+}
+
+
+provider "yandex" {
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.yc_default_zone
+  service_account_key_file = base64decode(var.yc_sa_key)
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "yc_cloud_id" {
+  type        = string
+  description = "Cloud Id"
+}
+
+variable "yc_folder_id" {
+  type        = string
+  description = "Folder Id"
+}
+
+variable "yc_default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default availability zone"
+}
+
+variable "yc_sa_key" {
+  type = string
+  sensitive = true
+  description = "Service Account key"
+}
+
+variable "yc_s3_bucket" {
+  type = string
+  sensitive = true
+  description = "Bucket for tfstate"
+}
