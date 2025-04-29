0cb84bcf1106849b6a5ab3a51dc2611cb9600fd5
Kashin Aleksey <kashin.aleksey@gmail.com>
2025-04-30 02:09:18 +0300

    commit

--- modules/compute_instance/app-alb.tf
+++ modules/compute_instance/app-alb.tf
@@ -33,12 +33,12 @@ resource "yandex_alb_virtual_host" "app_virtual_host" {
 
     route {
         name = "app-route"
-        # Условие совпадения пути
-        http_match {
-            path_prefix = "/app" # Запросы, начинающиеся с /app
-        }
-        # Действие маршрута
         http_route {
+            # Условие совпадения пути
+            http_match {
+                path_prefix = "/app" # Запросы, начинающиеся с /app
+            }
+            # Действие маршрута
             http_route_action {
                 backend_group_id = yandex_alb_backend_group.app_backend_group.id
             }
